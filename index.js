9a54a363edd85f8591e622ba04b7b56aa5591cf6
Matilda <ogarimatilda@gmail.com>
2024-04-17 18:41:47 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
 /* Your Code Here */
-
-/*
+function createEmployeeRecord(array) {
+    const [firstName, familyName, title, payPerHour] = array;
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    }
+  }
+  
+  
+  function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+  }
+  
+  
+  function createTimeInEvent(timeStamp) {
+    const [date, hour] = timeStamp.split(" ");
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date,
+    });
+    return this
+  }
+  
+  
+  function createTimeOutEvent(timeStamp) {
+    const [date, hour] = timeStamp.split(" ");
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date,
+    });
+    return this
+  }
+  
+  
+  function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find((event) => event.date === date)
+    const timeOutEvent = this.timeOutEvents.find((event) => event.date === date)
+  
+    if (timeInEvent && timeOutEvent) {
+      const startTime = parseInt(timeInEvent.hour);
+      const endTime = parseInt(timeOutEvent.hour);
+      return (endTime - startTime) / 100;
+    }
+  
+    return 0
+  }
+  
+  
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
+ 
+  /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
@@ -9,7 +67,13 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+  
+  function findEmployeeByFirstName(arrayOfEmployees, firstName) {
+    return arrayOfEmployees.find((employee) => employee.firstName === firstName)
+  }
+  
+  
+  const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -21,3 +85,34 @@ const allWagesFor = function () {
     return payable
 }
 
+  function calculatePayroll(array) {
+    return array.reduce((totalPayroll, person) => {
+      return totalPayroll + allWagesFor.call(person);
+    }, 0)
+  }
+  
+ 
+  
+  // Create employee records from data
+  const employeesData = [
+    ['Gray', 'Worm', 'Security', 1],
+    ['John', 'Doe', 'Manager', 2]
+  ];
+  const employees = createEmployeeRecords(employeesData);
+  
+  // Record time events for employees
+  createTimeInEvent.call(employees[0], "2024-04-17 0900")
+  createTimeOutEvent.call(employees[0], "2024-04-17 1700")
+  
+  createTimeInEvent.call(employees[1], "2024-04-17 1000")
+  createTimeOutEvent.call(employees[1], "2024-04-17 1800")
+  
+  
+  employees.forEach(employee => {
+    console.log(`${employee.firstName} ${employee.familyName} earned ${allWagesFor.call(employee)} today.`)
+  });
+  
+  
+  const totalPayroll = calculatePayroll(employees);
+  console.log(`Total payroll for today is ${totalPayroll}.`)
+  
